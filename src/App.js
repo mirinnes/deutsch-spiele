22508912907611f5a5b59bdfcfc3d4cdaaf20ba1
Miranda Inés Lopez <58486801+mirinnes@users.noreply.github.com>
2021-07-21 22:48:58 +0200

    Conditional of random bubbles colours and positions generated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="bubble" id="n1"></div>
+      <div className="bubble" id="n2"></div>
+      <div className="bubble" id="n3"></div>
+    </div>
+  );
 }
 
 export default App;
